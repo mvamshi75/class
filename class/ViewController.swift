177f4e46f3ff148a0ed06a74533dc205d55da8d6
M VAMSHI <mvamshi985@achiever-128.local>
2019-10-03 17:45:21 +0530

    Initial Commit

---
+++ class/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  class
+//
+//  Created by M VAMSHI on 10/3/19.
+//  Copyright © 2019 M VAMSHI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
